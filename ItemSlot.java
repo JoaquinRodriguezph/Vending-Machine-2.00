4c813ce3abc15575ba1fd7e4dbc9fc353ec93a3e
Quinn <joaquin_rodriguez@dlsu.edu.ph>
2023-06-17 23:28:55 +0800

    Buncha edits

--- ItemSlot.java
+++ ItemSlot.java
@@ -1,4 +1,15 @@
+/**
+ * The ItemSlot Class contains the slot number
+ * that contains a specific item type. The slot number
+ * and the max amount of items in a specific slot is final.
+ */
 public class ItemSlot {
+    /**
+     * This method is a constructor which creates an ItemSlot instance
+     * using the following parameters:
+     * @param slot the slot number to be inputted
+     * @param max the max amount of items in the slot.
+     */
     public ItemSlot(int slot, int max){
         this.SLOTNUMBER = slot;
         this.item = null;
@@ -6,22 +17,44 @@ public class ItemSlot {
         this.MAX = max;
     }
 
+    /**
+     * This method gets the slot Number.
+     * @return the slot number
+     */
     public int getSlotNumber() {
         return SLOTNUMBER;
     }
 
+    /**
+     * This method gets the item in the slot.
+     * @return the item in the Slot
+     */
     public Item getItem() {
         return item;
     }
 
+    /**
+     * This method gets the stock of the item in the slot.
+     * @return the stock
+     */
     public int getStock() {
         return stock;
     }
 
+    /**
+     * This method sets the item that is in the item slot.
+     * @param item the item to be set in the slot
+     */
     public void setItem(Item item) {
         this.item = item;
     }
 
+    /**
+     * This method adds the amount specified in the parameter if it is not
+     * below 0 and is not above the maximum capacity.
+     * @param stock the amount of stock to be added
+     * @return true or false depending on if the stocking is successful
+     */
     public boolean addStock(int stock){
         if (this.item != null && stock > 0 && MAX >= this.stock + stock){
             this.stock += stock;
@@ -33,6 +66,10 @@ public class ItemSlot {
         return false;
     }
 
+    /**
+     * The method checks if there is stock left of an item.
+     * @return true or false depending if there is at least one stock
+     */
     public boolean isAvailable() {
         return stock > 0;
     }
