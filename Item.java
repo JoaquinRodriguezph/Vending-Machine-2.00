7b1503e33b3fe405b6be26071f06ab8a7eab8eec
Quinn <joaquin_rodriguez@dlsu.edu.ph>
2023-06-17 23:19:40 +0800

    Buncha edits

--- Item.java
+++ Item.java
@@ -1,5 +1,18 @@
+/**
+ * The Item Class contains the information and methods to change
+ * the information of an item instance. There is a counter attribute
+ * as well which contains the total amount of items.
+ */
 public class Item {
     //peepee poo poo
+
+    /**
+     * This method is a constructor used to create an item instance
+     * from the following parameters:
+     * @param name This is the name of the item. IT IS FINAL.
+     * @param cost This is the cost of the item.
+     * @param calories This is the amount of calories in the item. IT IS FINAL.
+     */
     public Item(String name, int cost, int calories){
         this.NAME = name;
         this.cost = cost;
@@ -8,33 +21,62 @@ public class Item {
         count++;
     }
 
+    /**
+     * This method gets the name of the item.
+     * @return the name of the item
+     */
     public String getName() {
         return NAME;
     }
 
+    /**
+     * This method gets the amount of calories of the item.
+     * @return the number of calories of the item
+     */
     public int getCalories() {
         return CALORIES;
     }
 
+    /**
+     * This method gets the cost of the item.
+     * @return the price of the item.
+     */
     public int getCost() {
         return cost;
     }
 
+    /**
+     * This method gets the count of all items.
+     * @return the count of all items.
+     */
     public int getCount() {
         return count;
     }
 
+    /**
+     * This method gets the number of the item in stock.
+     * @return the stock
+     */
     public int getStock() {
         return stock;
     }
 
+    /**
+     * This method sets the number of items in stock of the item.
+     * @param stock the amount of item to add in stock.
+     */
     public void setStock(int stock) {
         this.stock = stock;
     }
 
+    /**
+     * This method adds the amount of items in stock of the item.
+     * @param stock the amount of item to add in stock.
+     */
     public void addStock(int stock) {
         this.stock += stock;
     }
+
     private final String NAME;
 
     private int cost;
