2a9ae007c00e7c07acf28cd7a602eab88816dda5
stanstan50 <stanley_yale_zeng@dlsu.edu.ph>
2023-06-17 13:23:26 +0800

    updates in the Item.java class

--- Item.java
+++ Item.java
@@ -1,16 +1,18 @@
 public class Item {
     //peepee poo poo
-    public Item(String name, int cost, float calories){
+    public Item(String name, int cost, int calories){
         this.NAME = name;
         this.cost = cost;
         this.CALORIES = calories;
+        stock = 0;
+        count++;
     }
 
     public String getName() {
         return NAME;
     }
 
-    public float getCalories() {
+    public int getCalories() {
         return CALORIES;
     }
 
@@ -18,9 +20,28 @@ public class Item {
         return cost;
     }
 
+    public int getCount() {
+        return count;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void addStock(int stock) {
+        this.stock += stock;
+    }
     private final String NAME;
 
     private int cost;
 
-    private final float CALORIES;
+    private final int CALORIES;
+
+    private int stock;
+
+    private static int count = 0;
 }
