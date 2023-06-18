396388dd12edca8b5c92317df782a6a797a177e9
Quinn <joaquin_rodriguez@dlsu.edu.ph>
2023-06-18 13:16:06 +0800

    Buncha edits

--- ItemTransaction.java
+++ ItemTransaction.java
@@ -1,16 +1,38 @@
+/**
+ * The ItemTransaction class counts the number of times
+ * the specific item has been sold since the last restock.
+ */
 public class ItemTransaction {
+    /**
+     * This method is a constructor that creates an ItemTransaction
+     * instance using the following parameter:
+     * @param item item to be counted
+     */
     public ItemTransaction(Item item){
         this.item = item;
     }
 //dangs
+
+    /**
+     * This method gets the item
+     * @return the item of the ItemTransaction instance
+     */
     public Item getItem() {
         return item;
     }
 
+    /**
+     * This method gets the quantity of the item
+     * @return the quantity of the item
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * This method gets the total cost of the items
+     * @return the total cost of the items
+     */
     public int getTotal() {
         return item.getCost() * quantity;
     }
