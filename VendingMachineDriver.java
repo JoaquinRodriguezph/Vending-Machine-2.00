7007a0db4b6758d3c8ee040054e126eac07949c9
Quinn <joaquin_rodriguez@dlsu.edu.ph>
2023-06-26 19:51:44 +0800

    Buncha edits

--- VendingMachineDriver.java
+++ VendingMachineDriver.java
@@ -1,7 +1,26 @@
-public class VendingMachineDriver {
-    Item item1 = new Item("Piattos", 30, 150);
-    public static void main(String args[]) {
+import java.util.ArrayList;
 
+public class VendingMachineDriver {
+    private ArrayList<Item> ItemList = new ArrayList<Item>();
+
+    //VendingMachineDriver(){this.ItemList = new ArrayList<>();}
+    public static void main(String args[]) {
+        VendingMachineDriver vm = new VendingMachineDriver();
+        vm.ItemList.add(new Item("Fried Egg", 30, 150));
+        vm.ItemList.add(new Item("Chippy", 75, 170));
+        vm.ItemList.add(new Item("Tapas", 100, 200));
+        vm.ItemList.add(new Item("Hotdog", 75, 290));
+        vm.ItemList.add(new Item("Piattos", 75, 150));
+        vm.ItemList.add(new Item("V-Cut", 75, 180));
+        vm.ItemList.add(new Item("Corn", 20, 88));
+        vm.ItemList.add(new Item("Fried Chicken", 100, 246));
+        vm.ItemList.add(new Item("Bangus", 100, 200));
+        vm.ItemList.add(new Item("Pork Chop", 100, 231));
+        vm.ItemList.add(new Item("Tosino", 100, 230));
+        vm.ItemList.add(new Item("Rice", 20, 206));
+        vm.ItemList.add(new Item("Coca Cola", 50, 139));
+        vm.ItemList.add(new Item("Royal", 50, 139));
+        vm.ItemList.add(new Item("Bottled Water", 25, 0));
     }
 
 }
