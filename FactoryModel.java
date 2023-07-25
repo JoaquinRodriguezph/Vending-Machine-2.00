ae4053cc456f7f39c3971bf8585e1532d0122761
Quinn <joaquin_rodriguez@dlsu.edu.ph>
2023-07-25 23:17:01 +0800

    Gooey

--- FactoryModel.java
+++ FactoryModel.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+
 public class FactoryModel {
+    private ArrayList<VendingMachine> vendingMachines;
+
+    public FactoryModel(){this.vendingMachines = new ArrayList<VendingMachine>();}
+
 }
--- FactoryView.java
+++ FactoryView.java
@@ -1,26 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class FactoryView {
     private JFrame mainFrame;
     private JLabel menuLabel;
     private JPanel btnPanel;
+    private JButton selectBtn, createBtn, showBtn;
     private ArrayList<JButton> buttonList;
 
     public FactoryView() {
         this.btnPanel = new JPanel();
         btnPanel.setLayout(new GridLayout(10,1));
+        this.buttonList = new ArrayList<JButton>();
 
         this.mainFrame = new JFrame("Vending Machine Factory");
         this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.mainFrame.setLayout(new FlowLayout(FlowLayout.CENTER));
         this.mainFrame.setSize(350, 400);
-
-        this.menuLabel = new JLabel("Main Menu");
-        menuLabel.setHorizontalAlignment(JLabel.CENTER);
-
-        this.mainFrame.add(this.menuLabel);
 
         setProgramMenuGrid();
 
@@ -31,13 +29,29 @@ public class FactoryView {
 
     public void setProgramMenuGrid() {
 
-        JButton btn = new JButton();
-        btn.setPreferredSize(new Dimension(300, 10));
-        btn.setLabel("12");
+        //JButton btn = new JButton();
+        //btn.setPreferredSize(new Dimension(300, 10));
+        //btn.setLabel("12");
 
-        btnPanel.add(new Button("Start Main Program"));
-        btnPanel.add(btn);
-        btnPanel.add(new Button("1"));
+        this.menuLabel = new JLabel("Main Menu");
+        menuLabel.setVerticalAlignment(JLabel.TOP);
+        this.mainFrame.add(this.menuLabel);
 
+        buttonList.add(new JButton("Start Main Program"));
+        buttonList.add(new JButton("Customize Vending Machines"));
+        buttonList.add(new JButton("Customize Money"));
+        buttonList.add(new JButton("Customize Item Stocks"));
+        buttonList.add(new JButton("Show Inventory"));
+
+        for (JButton button : buttonList){
+            button.setBackground(Color.pink);
+            button.setVerticalTextPosition(SwingConstants.CENTER);
+            button.setHorizontalTextPosition(SwingConstants.CENTER);
+            btnPanel.add(button);
+        }
     }
+
+    public void setSelectBtnListener(ActionListener actionListener){this.selectBtn.addActionListener(actionListener);}
+    public void setCreateBtnListener(ActionListener actionListener){this.createBtn.addActionListener(actionListener);}
+    public void setShowBtnListener(ActionListener actionListener){this.showBtn.addActionListener(actionListener);}
 }
